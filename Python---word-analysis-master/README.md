da22484e848dba65e397a15a27e647680512b9d2
pranavjoy <pranavjoy@protonmail.com>
2017-10-26 14:31:44 +0200

    changes

--- Python---word-analysis-master/README.md
+++
@@ -1,2 +0,0 @@
-# Python---word-analysis
-A program to count the number of occurrences of words in a text.
--- word-analyser.py
+++ word-analyser.py
@@ -1,16 +1,16 @@
-import operator
-my_file= open("glossary.txt", "r", encoding = 'utf-8').read()
-list_count={
+    import operator
+    my_file= open("glossary.txt", "r", encoding = 'utf-8').read()
+    list_count={
 
-}
-list=my_file.replace(".", "").replace("'", "").replace("?", "").replace(":", "").replace("\"", "").replace("\n", " ").replace(",", "").split(" ")
-for element in list:
-    if element in list_count.keys():
-        list_count[element]+= 1
-    else:
-        list_count[element]=1
-sorted_list_count=sorted(list_count.items(), key=operator.itemgetter(1))
-final=sorted_list_count[::-1]
-for word in final:
-    print (f'{word[0]} : {word[1]}')
-#END
+    }
+    list=my_file.replace(".", "").replace("'", "").replace("?", "").replace(":", "").replace("\"", "").replace("\n", " ").replace(",", "").split(" ")
+    for element in list:
+        if element in list_count.keys():
+            list_count[element]+= 1
+        else:
+            list_count[element]=1
+    sorted_list_count=sorted(list_count.items(), key=operator.itemgetter(1))
+    final=sorted_list_count[::-1]
+    for word in final:
+        print (f'{word[0]} : {word[1]}')
+    #END
