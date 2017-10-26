5af4da28e37a154eb2f3c6a7914ade428e7d99ab
pranavjoy <pranavjoy@protonmail.com>
2017-10-26 14:21:59 +0200

    Word

---
+++ word-analyserf.py
@@ -0,0 +1,15 @@
+import operator
+my_file= open("glossary.txt", "r", encoding = 'utf-8').read()
+list_count={
+
+}
+list=my_file.replace(".", "").replace("'", "").replace("?", "").replace(":", "").replace("\"", "").replace("\n", " ").replace(",", "").split(" ")
+for element in list:
+    if element in list_count.keys():
+        list_count[element]+= 1
+    else:
+        list_count[element]=1
+sorted_list_count=sorted(list_count.items(), key=operator.itemgetter(1))
+final=sorted_list_count[::-1]
+for word in final:
+    print (f'{word[0]} : {word[1]}')
